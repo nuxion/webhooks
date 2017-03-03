89cdc3cdf291893be1db761b76c0ae59771eded9
xavier petit <nuxion@gmail.com>
2017-03-03 00:56:35 -0300

    refactoring issue #1

--- webhooks.py
+++ webhooks.py
@@ -2,32 +2,18 @@ from flask import Flask, request, jsonify
 import json
 
 app = Flask(__name__)
-#@app.route('/bitbucket/<repositorio>', methods = ['POST'])
-@app.route('/bitbucket/<repositorio>', methods = ['POST', 'GET'])
-def test(repositorio):
+@app.route('/bitbucket/<repositorio>', methods = ['POST'])
+def getRep(repositorio):
+    
+    # validate request
     if request.method=='POST': 
         resp = str(repositorio)
     else:
         resp = "invalid"
-    #return 'repositorio %s addr %s \n' % resp, request.remote_addr
-    data = request.get_json()
-    tipo = type(data[0])
-    with open('request.log', 'a') as out:
-        #out.write(str(request.json))
-        out.write(str(tipo))
+    
+    #data = request.get_json()
 
-    #return 'add %s \n' % request.headers 
-    #return jsonify(json.loads(request.data))
-    #return jsonify(request.json)
-    #print (data[0])    
-    return str(data[0])
-    #return jsonify(data)
-    #return 'user: %s \n' % user
+    return 'resp: %s \n' % resp
 
-@app.route('/post/<int:post_id>')
-def show_post(post_id):
-    # show the post with the given id, the id is an integer
-    print (post_id)
-    #return 'Post %d' % post_id
 if __name__ == "__main__" :
     app.run(port=5000,debug = True)
